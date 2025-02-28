9f9177f8f39199efcf8f6ac1d4414a3dbe7645a1
Victor Matos <vmatos@elcamino.edu>
2025-02-28 08:43:51 -0800

    Comments added

--- C13-Rational-LAB.cpp
+++ C13-Rational-LAB.cpp
@@ -50,7 +50,8 @@ public:
         setDenominator(den);
     }
 
-    // Copy Constructor
+    // Copy Constructor - List Ininitalizer - Lazy constructor style
+    // NOT really needed - there are no pointers (only for illustration)
     Rational(const Rational& other) : numerator(other.numerator), 
         denominator(other.denominator) {}
 
@@ -166,54 +167,56 @@ public:
 
 };
 
-
+// ===========================================================================
 int main() {
     Rational r1(3, 4);
     Rational r2;
 
-    cout << "r1: " << r1.toString() << endl; // Output: r1: 3/4
-    cout << "r2: " << r2.toString() << endl; // Output: r2: 1/1
+    cout << "r1: " << r1.toString() << endl;    // Output: r1: 3/4
+    cout << "r2: " << r2.toString() << endl;    // Output: r2: 1/1
 
     r2.setNumerator(1);
     r2.setDenominator(2);
-    cout << "r2: " << r2.toString() << endl; // Output: r2: 1/2
+    cout << "r2: " << r2.toString() << endl;    // Output: r2: 1/2
 
 
     Rational sum = r1.add(r2);
-    cout << "sum: " << sum.toString() << endl;   // Output: sum: 5/4
+    cout << "sum: " << sum.toString() << endl;  // Output: sum: 5/4
 
     Rational diff = r1.subtract(r2);
     cout << "diff: " << diff.toString() << endl; // Output: diff: 1/4
 
     Rational r3(-2, 3);
-    cout << "r3: " << r3.toString() << endl; // Output: r3: -2/3
+    cout << "r3: " << r3.toString() << endl;    // Output: r3: -2/3
 
     Rational r4(40, -60);
-    cout << "r4: " << r4.toString() << endl; // Output: r4: -2/3
+    cout << "r4: " << r4.toString() << endl;    // Output: r4: -2/3
 
     Rational r5(r4);
-    cout << "r5: " << r5.toString() << endl; // Output: r5: -2/3
+    cout << "r5: " << r5.toString() << endl;    // Output: r5: -2/3
 
-    cout << "GCD of 40 and 60: " << Rational::gcd(40, 60) << endl; // Output: 20
+    cout << "GCD of 40 and 60: "                // Call static function
+         << Rational::gcd(40, 60) << endl;      // Output: 20
 
-    r5.setDenominator(0); // Output: Error: Denominator cannot be zero. Setting to 1.
-    cout << "r5: " << r5.toString() << endl; // Output: r5: -2/1
+    r5.setDenominator(0);                       // Output: Error-Denominator-cannot-be-zero 
+                                                // setting to 1.
+    cout << "r5: " << r5.toString() << endl;    // Output: r5: -2/1
 
     r4.setDenominator(-10);
-    cout << "r4: " << r4.toString() << endl; // Output: r4: 1/5
+    cout << "r4: " << r4.toString() << endl;    // Output: r4: 1/5
 
     //Test operator overloading
     Rational r6 = r1 + r2;
-    cout << "r6: " << r6.toString() << endl; // Output: r6: 5/4
+    cout << "r6: " << r6.toString() << endl;    // Output: r6: 5/4
 
     Rational r7 = r1 - r2;
-    cout << "r7: " << r7.toString() << endl; // Output: r7: 1/4
+    cout << "r7: " << r7.toString() << endl;    // Output: r7: 1/4
 
-    cout << "r1: " << r1 << endl; // Output: r1: 3/4    
-    cout << "r2: " << r2 << endl; // Output: r2: 1/2
+    cout << "r1: " << r1 << endl;               // Output: r1: 3/4    
+    cout << "r2: " << r2 << endl;               // Output: r2: 1/2
 
-    Rational r8 = 5 + r1;    //friend operator+
-    cout << "r8: " << r8 << endl; // Output: r8: 23/4
+    Rational r8 = 5 + r1;                       // friend operator+
+    cout << "r8: " << r8 << endl;               // Output: r8: 23/4
 
     Rational r9 = r1 + 5;   // The condensed constructor will convert int 5
                             // to Rational(5, 1), then uses the provided
@@ -221,8 +224,5 @@ int main() {
     cout << "r9 " << r9 << endl;
 
     //TODO - Add more tests for the remaining operators
-    return 0;
-
-
 }
 
